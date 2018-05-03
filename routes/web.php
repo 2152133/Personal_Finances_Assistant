b40ebe2178c15943262e90fd3d136404d0e09914
André Agostinho Nogueira <2151580@my.ipleiria.pt>
2018-05-03 11:22:00 +0100

    us1 us2 us3

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('navBar_footer.navBar');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
