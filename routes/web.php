da796114b5ac035a1b9a83f13cbd03bfd5689984
André Agostinho Nogueira <2151580@my.ipleiria.pt>
2018-05-17 13:05:12 +0100

    arganizacao de codigo (rotas)

--- routes/web.php
+++ routes/web.php
@@ -11,46 +11,42 @@
 |
 */
 
+
+
+// US1 -> main page
 Route::get('/', function () {	
     return view('welcome');
 });
 
+// US2,3,4,8 -> register, login, forgot password, logout
 Auth::routes();
 
-
-
-
-/*US14*/
-Route::get('/accounts/start', 'AccountsController@getAllAccountsStart');
-Route::get('/accounts/{user}', 'AccountsController@getAllAccountsFromUser');
-
-/*US26*/
-Route::get('/me/dashboard', 'DashboardController@index');
-
-
+// US5-6 -> list users profiles (admin)
 Route::get('/users', 'UserController@index')->middleware('can:administrate')->name('users');
 
-
-
+//US7 -> operacoes no utilizador
 Route::patch('/users/{user}/block', 'UserController@block');
 Route::patch('/users/{user}/unblock', 'UserController@unblock');
 Route::patch('/users/{user}/promote', 'UserController@promote');
 Route::patch('/users/{user}/demote', 'UserController@demote');
 
-
-
-// home page
-Route::get('/home', 'HomeController@index')->name('home');
-
-// change user password
+// US9 -> change user password
 Route::get('/me/password', 'UserController@editPassword')->name('user.editPassword');
 Route::patch('/me/password', 'UserController@updatePassword')->name('user.updatePassword');
 
-// update user profile
+// US10 -> update user profile
 Route::get('/me/profile', 'UserController@editProfile')->name('user.editProfile');
 Route::put('/me/profile', 'UserController@updateProfile')->name('user.updateProfile');
 
-// list profiles
+// US11 -> list users profiles (users)
 Route::get('/profiles', 'UserController@listProfiles')->name('user.listProfiles');
+
+// US14 -> accounts
+Route::get('/accounts/start', 'AccountsController@getAllAccountsStart');
+Route::get('/accounts/{user}', 'AccountsController@getAllAccountsFromUser');
+
+// US26 user dashboard page
+Route::get('/me/dashboard', 'DashboardController@index');
+
 
 
